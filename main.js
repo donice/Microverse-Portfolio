fe71a325a7b05aefec2e00bda5e252cf83f670e4
Jonathan Arias <129122417+sinruidotec@users.noreply.github.com>
2023-07-25 18:50:57 -0500

    Update main.js

--- main.js
+++ main.js
@@ -14,3 +14,17 @@ mobileNavClose.addEventListener('click', toggleMobileNav);
 navLinks.forEach((link) => {
   link.addEventListener('click', toggleMobileNav);
 });
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const target = document.querySelector(event.target.getAttribute("href"));
+  if (target) {
+    target.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+const anchorTags = document.querySelectorAll('a[href^="#"]');
+
+anchorTags.forEach((anchor) => {
+  anchor.addEventListener("click", scrollToSection);
+});
