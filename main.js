679765580a83ba13cbfe9f893d217c1ba0dd3ea7
Donice <technocrater.pdevelopers@gmail.com>
2023-07-26 00:52:09 +0100

    Done

--- main.js
+++ main.js
@@ -1,30 +1,16 @@
-const hamburger = document.querySelector('.hamburger');
-const mobileNav = document.querySelector('.mobile-nav');
-const mobileNavClose = document.querySelector('#close');
-const navLinks = document.querySelectorAll('.nav-links');
+const hamburger = document.querySelector(".hamburger");
+const mobileNav = document.querySelector(".mobile-nav");
+const mobileNavClose = document.querySelector("#close");
+const navLinks = document.querySelectorAll(".nav-links");
 
 function toggleMobileNav() {
-  hamburger.classList.toggle('is-active');
-  mobileNav.classList.toggle('is-active');
+  hamburger.classList.toggle("is-active");
+  mobileNav.classList.toggle("is-active");
 }
 
-hamburger.addEventListener('click', toggleMobileNav);
-mobileNavClose.addEventListener('click', toggleMobileNav);
+hamburger.addEventListener("click", toggleMobileNav);
+mobileNavClose.addEventListener("click", toggleMobileNav);
 
 navLinks.forEach((link) => {
-  link.addEventListener('click', toggleMobileNav);
-});
-
-function scrollToSection(event) {
-  event.preventDefault();
-  const target = document.querySelector(event.target.getAttribute("href"));
-  if (target) {
-    target.scrollIntoView({ behavior: "smooth" });
-  }
-}
-
-const anchorTags = document.querySelectorAll('a[href^="#"]');
-
-anchorTags.forEach((anchor) => {
-  anchor.addEventListener("click", scrollToSection);
+  link.addEventListener("click", toggleMobileNav);
 });
