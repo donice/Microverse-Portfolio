4f5e50a8ed56061511f062e67a1c8335a647e3e7
Donice <technocrater.pdevelopers@gmail.com>
2023-07-26 00:59:18 +0100

    Fixed Linters errors

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const hamburger = document.querySelector(".hamburger");
-const mobileNav = document.querySelector(".mobile-nav");
-const mobileNavClose = document.querySelector("#close");
-const navLinks = document.querySelectorAll(".nav-links");
+const hamburger = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.mobile-nav');
+const mobileNavClose = document.querySelector('#close');
+const navLinks = document.querySelectorAll('.nav-links');
 
 function toggleMobileNav() {
-  hamburger.classList.toggle("is-active");
-  mobileNav.classList.toggle("is-active");
+  hamburger.classList.toggle('is-active');
+  mobileNav.classList.toggle('is-active');
 }
 
-hamburger.addEventListener("click", toggleMobileNav);
-mobileNavClose.addEventListener("click", toggleMobileNav);
+hamburger.addEventListener('click', toggleMobileNav);
+mobileNavClose.addEventListener('click', toggleMobileNav);
 
 navLinks.forEach((link) => {
   link.addEventListener('click', toggleMobileNav);
