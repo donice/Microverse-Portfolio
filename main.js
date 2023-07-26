e330c20dac8cf98bdb9d326380f4979bd3ea1d28
Donice <technocrater.pdevelopers@gmail.com>
2023-07-26 20:51:32 +0100

    Initiated projects object

--- main.js
+++ main.js
@@ -28,3 +28,82 @@ const anchorTags = document.querySelectorAll('a[href^="#"]');
 anchorTags.forEach((anchor) => {
   anchor.addEventListener('click', scrollToSection);
 });
+
+const projects = [
+  {
+    name: "Tonic",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    featured: {
+      name: "CANOPY",
+      stack: "Back End Dev",
+      year: 2018,
+    },
+    imageMobile: "./images/project-1.svg",
+    imageDesktop: "./images/project-1-dt.svg",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    liveLink: "drdonice.dev",
+    srcLink: "github.com",
+    inversed: false,
+  },
+  {
+    name: "Tonic",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    featured: {
+      name: "CANOPY",
+      stack: "Back End Dev",
+      year: 2018,
+    },
+    imageMobile: "./images/project-2.svg",
+    imageDesktop: "./images/project-2-dt.svg",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    liveLink: "drdonice.dev",
+    srcLink: "github.com",
+    inversed: true,
+  },
+  {
+    name: "Tonic",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    featured: {
+      name: "CANOPY",
+      stack: "Back End Dev",
+      year: 2018,
+    },
+    imageMobile: "./images/project-3.svg",
+    imageDesktop: "./images/project-3-dt.svg",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    liveLink: "drdonice.dev",
+    srcLink: "github.com",
+    inversed: false,
+  },
+  {
+    name: "Tonic",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    featured: {
+      name: "CANOPY",
+      stack: "Back End Dev",
+      year: 2018,
+    },
+    imageMobile: "./images/project-4.svg",
+    imageDesktop: "./images/project-4-dt.svg",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    liveLink: "drdonice.dev",
+    srcLink: "github.com",
+    inversed: true,
+  },
+]
